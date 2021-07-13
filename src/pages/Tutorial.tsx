8d01f235006c96080ecfc1393b2de94523051666
Bhanu P Chaudhary <bhanu423@gmail.com>
2021-07-14 02:43:14 +0530

    updated tutorial

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -49,32 +49,33 @@ const Tutorial: React.FC<TutorialProps> = ({ history, setHasSeenTutorial, setMen
           <IonSlide>
             <img src="assets/img/ica-slidebox-img-1.png" alt="" className="slide-image" />
             <h2 className="slide-title">
-              Welcome to <b>ICA</b>
+              Welcome to <b>Kado</b>
             </h2>
             <p>
-              The <b>ionic conference app</b> is a practical preview of the ionic framework in action, and a demonstration of proper code use.
+              We are India's largest health events and community app. You are not alone.
             </p>
           </IonSlide>
 
           <IonSlide>
             <img src="assets/img/ica-slidebox-img-2.png" alt="" className="slide-image" />
-            <h2 className="slide-title">What is Ionic?</h2>
+            <h2 className="slide-title">What is Kado?</h2>
             <p>
-              <b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.
+              <b>Kado</b> is an unique health community managed by doctors, to ensure you get the most
+              credible and authentic information from our application.
             </p>
           </IonSlide>
 
           <IonSlide>
             <img src="assets/img/ica-slidebox-img-3.png" alt="" className="slide-image" />
-            <h2 className="slide-title">What is Ionic Appflow?</h2>
+            <h2 className="slide-title">What can you do on Kado?</h2>
             <p>
-              <b>Ionic Appflow</b> is a powerful set of services and features built on top of Ionic Framework that brings a totally new level of app development agility to mobile dev teams.
+              <b>You</b> can connect with other patients facing similar problems. You can also attend our live events hosted by Real Doctors and get your minor queries in realtime.
             </p>
           </IonSlide>
 
           <IonSlide>
             <img src="assets/img/ica-slidebox-img-4.png" alt="" className="slide-image" />
-            <h2 className="slide-title">Ready to Play?</h2>
+            <h2 className="slide-title">Ready to make the change?</h2>
             <IonButton fill="clear" onClick={startApp}>
               Continue
               <IonIcon slot="end" icon={arrowForward} />
